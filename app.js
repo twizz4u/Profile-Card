d1003affcfe177f5a4c3ff59f6f96b2163787ace
twizz4u <tiwatayoaragbaiye@gmail.com>
2025-10-21 21:39:10 +0100

    updated profile card to include contact and page

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-document.querySelectorAll(".time").forEach((element) => {
-  const milliiseconds = Date.now();
-  element.textContent = `Current time: ${milliiseconds}`;
-});
-
-const inputs = Array.from(document.querySelectorAll("input"));
-const small = Array.from(document.querySelectorAll("small"));
-const textarea = document.querySelector("textarea");
-const success = document.querySelector(".success");
-
-const errormessges = ["name", "email", "subject", "textarea"];
-
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector("#contactForm").addEventListener("submit", (e) => {
+  document.querySelectorAll(".time").forEach((element) => {
+    const milliiseconds = Date.now();
+    element.textContent = `Current time: ${milliiseconds}`;
+  });
+
+  const inputs = Array.from(document.querySelectorAll("input"));
+  const small = Array.from(document.querySelectorAll("small"));
+  const textarea = document.querySelector("textarea");
+  const success = document.querySelector(".success");
+  const form = document.getElementById("contactForm");
+
+  const errormessges = ["name", "email", "subject", "textarea"];
+
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
     isvalid = true;
     let firstInvalid = null;
@@ -71,48 +72,48 @@ document.addEventListener("DOMContentLoaded", function () {
       success.classList.remove("add");
     }, 1200);
   });
-});
 
-inputs.forEach((input, index) => {
-  input.addEventListener("keyup", (e) => {
-    if (input.value.length < 10) {
-      input.style.borderColor = "rgb(231, 127, 127)";
-      small[
-        index
-      ].textContent = `*Input ${errormessges[index]} requires at least 10 characters`;
-    } else {
-      input.style.borderColor = "rgba(160, 219, 136, 1)";
-      small[index].textContent = "";
-    }
-
-    if (input.id == "email") {
-      if (
-        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
-          input.value.trim()
-        )
-      ) {
-        input.style.borderColor = "rgba(160, 219, 136, 1)";
-      } else {
+  inputs.forEach((input, index) => {
+    input.addEventListener("keyup", (e) => {
+      if (input.value.length < 10) {
         input.style.borderColor = "rgb(231, 127, 127)";
-        small[index].textContent = `*Please enter a valid email address`;
+        small[
+          index
+        ].textContent = `*Input ${errormessges[index]} requires at least 10 characters`;
+      } else {
+        input.style.borderColor = "rgba(160, 219, 136, 1)";
+        small[index].textContent = "";
       }
+
+      if (input.id == "email") {
+        if (
+          /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
+            input.value.trim()
+          )
+        ) {
+          input.style.borderColor = "rgba(160, 219, 136, 1)";
+        } else {
+          input.style.borderColor = "rgb(231, 127, 127)";
+          small[index].textContent = `*Please enter a valid email address`;
+        }
+      }
+    });
+  });
+
+  textarea.addEventListener("keyup", (e) => {
+    console.log(textarea);
+    console.log(textarea.value.length);
+    if (textarea.value.trim() == "") {
+      console.log(textarea.value);
+      textarea.style.borderColor = "rgb(231, 127, 127)";
+    } else if (textarea.value.length < 10) {
+      textarea.style.borderColor = "rgb(231, 127, 127)";
+      small[
+        inputs.length
+      ].textContent = `*message must be at least 10 characters`;
+    } else {
+      textarea.style.borderColor = "rgba(160, 219, 136, 1)";
+      small[inputs.length].textContent = "";
     }
   });
-});
-
-textarea.addEventListener("keyup", (e) => {
-  console.log(textarea);
-  console.log(textarea.value.length);
-  if (textarea.value.trim() == "") {
-    console.log(textarea.value);
-    textarea.style.borderColor = "rgb(231, 127, 127)";
-  } else if (textarea.value.length < 10) {
-    textarea.style.borderColor = "rgb(231, 127, 127)";
-    small[
-      inputs.length
-    ].textContent = `*message must be at least 10 characters`;
-  } else {
-    textarea.style.borderColor = "rgba(160, 219, 136, 1)";
-    small[inputs.length].textContent = "";
-  }
 });
